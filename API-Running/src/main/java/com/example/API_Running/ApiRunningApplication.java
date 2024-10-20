aaec08032555cce9bb6cd8739cf40563573f23c0
AdrianContre <adrian.contreras.martin@gmail.com>
2024-10-20 10:34:24 +0200

    First commit

---
+++ API-Running/src/main/java/com/example/API_Running/ApiRunningApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_Running;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRunningApplication.class, args);
+	}
+
+}
---
+++ API-Running/src/test/java/com/example/API_Running/ApiRunningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_Running;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRunningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
