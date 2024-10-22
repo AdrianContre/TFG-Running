49eb6b9673b289169d9974d1c9b052c2e7dc1471
AdrianContre <adrian.contreras.martin@gmail.com>
2024-10-22 16:13:45 +0200

    test Login

--- API-Running/src/test/java/com/example/API_Running/LoginTests.java
+++ API-Running/src/test/java/com/example/API_Running/LoginTests.java
@@ -1,54 +1,47 @@
 package com.example.API_Running;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.example.API_Running.controllers.AuthController;
-import com.example.API_Running.dtos.RegisterRequest;
+import com.example.API_Running.dtos.LoginRequest;
+import com.example.API_Running.models.Runner;
+import com.example.API_Running.models.User;
 import com.example.API_Running.repository.UserRepository;
 import com.example.API_Running.services.AuthService;
 import com.example.API_Running.services.JwtService;
 import com.example.API_Running.services.UserDetailsServiceImplementation;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.annotation.SecurityTestExecutionListeners;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
-public class RegisterTests {
-
+public class LoginTests {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    private static final String END_POINT = "/api/v1/auth/register";
+    private static final String END_POINT = "/api/v1/auth/login";
 
     @MockBean
     private AuthService authService;
@@ -65,43 +58,39 @@ public class RegisterTests {
     @MockBean
     UserDetailsServiceImplementation userDetailsServiceImplementation;
 
-    @MockBean
-    private WebApplicationContext webApplicationContext;
-
+    private Runner testUser;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        testUser = new Runner();
+        testUser.setName("test");
+        testUser.setUsername("test");
+        testUser.setPassword(new BCryptPasswordEncoder().encode("1234"));
+        testUser.setMail("test@example.com");
+        testUser.setSurname("test");
+        testUser.setWeight(70);
+        testUser.setHeight(180);
+        testUser.setFcMax(200);
+        testUser.setIsTrainer(false);
     }
 
     @Test
     @WithMockUser
-    public void testRegisterSuccess() throws Exception {
-        RegisterRequest request = new RegisterRequest();
-        request.setName("Test");
-        request.setSurname("Test surname");
+    public void testLoginSuccess() throws Exception {
+        LoginRequest request = new LoginRequest();
         request.setUsername("test");
-        request.setMail("test@gmail.com");
         request.setPassword("1234");
-        request.setWeight(70);
-        request.setHeight(180);
-        request.setFcMax(180);
-        request.setTrainer(false);
 
         String requestBody = objectMapper.writeValueAsString(request);
-
 
         mockMvc.perform(post(END_POINT)
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
                 .andExpect(status().isOk());
-
-
     }
 
 
 }
-
 
 
