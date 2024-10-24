87d88f818c05318289d07b9b769d673b054315e8
AdrianContre <adrian.contreras.martin@gmail.com>
2024-10-24 13:39:33 +0200

    Vista principal

--- frontend/src/modules/home/components/Main.jsx
+++ frontend/src/modules/home/components/Main.jsx
@@ -1,8 +1,18 @@
 import { useNavigate } from 'react-router';
+import { getUserLogged } from '../services/mainService';
 import NavigationBar from './NavigationBar';
+import { useEffect } from 'react';
 
 const Main = () => {
     const navigate = useNavigate()
+
+    useEffect (() => {
+        const getUserAuth = async () => {
+            const user = await getUserLogged();
+            localStorage.setItem('userAuth', JSON.stringify(user));
+        }
+        getUserAuth()
+    },[])
     
     return (
         <>
---
+++ frontend/src/modules/home/services/mainService.js
@@ -0,0 +1,27 @@
+const API_ROUTE = "http://localhost:8081/api/v1/users/whoAmI"
+
+export const getUserLogged = async () => {
+    const authHeader = `Bearer ${localStorage.getItem('token')}`
+    console.log(authHeader)
+    return fetch(API_ROUTE,{
+        method: 'GET',
+        headers: {
+        'Authorization': authHeader,
+        'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        // if (!response.ok) {
+        //   throw new Error(response);
+        // }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        return data.userLogged;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        throw error; // Lanzar el error para que pueda ser manejado por quien llame a esta función
+      });
+}
